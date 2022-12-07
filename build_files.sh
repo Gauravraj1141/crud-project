2399dcb6780cdd92eb3568caf27344a222bac98c
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2022-12-07 16:52:02 +0530

    add vercel2

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 echo "BUILD START"
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic  --noinput --clear
+
+pip install -r requirements.txt
+python3.10 manage.py collectstatic
 echo "BUILD END"
